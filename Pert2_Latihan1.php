270b51eded0eb8783b1f940af0e11e80895d14bd
chottedifs <husadif.aldi@gmail.com>
2021-03-12 16:19:34 +0700

    Commit Kedua Mengupload Tugas pada Pertemuan 2

---
+++ Pert2_Latihan1.php
@@ -0,0 +1,13 @@
+<?php 
+    $a = 1;
+    $b = 2;
+    $c = $a.$b;
+    // Menggabungkan $c sekarang 12, Menggabungkan angka 1 dan 2.
+    $d = $c + 1;
+    echo $d;
+    // Nilai $d 13
+    $e = "Number";
+    $f = $e.$d;
+    echo $f;
+    // Nilai $f menjadi Number13, a kombinasi $e dan $f
+?>
